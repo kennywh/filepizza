cca87aa096b67e69a6671661af2183db5186bc19
kenny <kenny.ited@gmail.com>
2025-03-09 17:47:36 +0800

    Update package.json and scripts, remove tests workflow

--- scripts/pull-and-run.sh
+++ scripts/pull-and-run.sh
@@ -3,6 +3,6 @@
 set -euo pipefail
 
 git pull origin main
-sudo docker pull kern/filepizza:latest
-sudo docker compose -f docker-compose.production.yml up -d
+sudo docker pull kennychau/fileshare:latest
+sudo docker compose -f docker-compose.yml up -d
 sudo docker compose logs -f
